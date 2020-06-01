bcf54900c38d681ca2728654c879e3bc23f23079
lucia raschetti <lucia.sn011@gmail.com>
2020-06-01 03:23:22 -0300

    LPPA-SPACESHIP: added pause & enemies

--- js/spaceship.js
+++ js/spaceship.js
@@ -6,12 +6,14 @@ var currentScene = 0;
 var scenes = [];
 var canvas = null;
 var context = null;
+var pause = true;
 var pressing = [];
 var lastPress = null;
 var ship = null;
 var shipImage = new Image();
 var shots = [];
 var shotImage = new Image();
+var enemies = [];
 var keyLeft = 37;
 var keyUp = 38;
 var keyRight = 39;
@@ -19,6 +21,10 @@ var keyDown = 40;
 var keyEnter = 13;
 var keySpace = 32;
 
+var random = function(max) {
+    return ~~(Math.random() * max);
+}
+    
 var Rectangle = function(x, y, width, height) { //rectangle obj 
     this.x = (x === undefined) ? 0 : x;
     this.y = (y === undefined) ? 0 : y;
@@ -32,7 +38,28 @@ var Rectangle = function(x, y, width, height) { //rectangle obj
             context.fillRect(this.x, this.y, this.width, this.height);
         }
     }
+
+    this.intersects = function(rect) {
+        if(rect === undefined) {
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+            return(this.x < rect.x + rect.width &&
+                this.x + this.width > rect.x &&
+                this.y < rect.y + rect.height &&
+                this.y + this.height > rect.y);
+        }
+    }
 }
+
+//browser compatibility
+window.requestAnimationFrame = (function() {
+    return window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    function(callback) {
+        window.setTimeout(callback, 17);
+    }
+}());
 
 document.addEventListener('keydown', function(evt) {
     lastPress = evt.keyCode;
@@ -79,11 +106,12 @@ menu = new scene();
 menu.paint = function(context) {
     context.fillStyle = '#201827';
     context.fillRect(0, 0, canvas.width, canvas.height);
-
+    
     context.fillStyle = '#ff0080a1';
     context.textAlign = 'center';
     context.fillText('SPACESHIP GAME', 150, 60);
-    context.fillText('Press Enter', 150, 90);
+    context.fillText('Use space & arrow keys to play', 150, 90);
+    context.fillText('Press Enter', 150, 120);
 };
 
 menu.act = function() {
@@ -95,25 +123,53 @@ menu.act = function() {
 
 game = new scene();
 
+game.load = function() {
+    ship = new Rectangle(145, 130, 16, 15);
+    shots.length = 0;
+    enemies.length = 0;
+    enemies.push(new Rectangle(10,0,10,10));
+    pause = true;
+}
+
 game.act = function() {
-    //horizontal movement
-    if(pressing[keyRight]) {ship.x += 10;}
-    if(pressing[keyLeft]) {ship.x -= 10;}
-    //canvas limits
-    if(ship.x > canvas.width - ship.width) {ship.x = canvas.width - ship.width;}
-    if(ship.x < 0) {ship.x = 0;}
-    //space -> adds new 'shot' to array
-    if(lastPress === keySpace) {
-        shots.push(new Rectangle(ship.x + 3, ship.y, 5, 5));
+    //pause
+    if(lastPress === keyEnter) {
+        pause =! pause;
         lastPress = null;
     }
-    //shots movement & removal when hitting the top of the canvas
-    for(var i = 0, l = shots.length; i < l; i++) {
-        shots[i].y -= 10;
-        if(shots[i].y < 0) {
-            shots.splice(i--, 1);
-            l--;
+    if(!pause){
+        //horizontal movement
+        if(pressing[keyRight]) {ship.x += 10;}
+        if(pressing[keyLeft]) {ship.x -= 10;}
+        //canvas limits
+        if(ship.x > canvas.width - ship.width) {ship.x = canvas.width - ship.width;}
+        if(ship.x < 0) {ship.x = 0;}
+        //space -> adds new 'shot' to array
+        if(lastPress === keySpace) {
+            shots.push(new Rectangle(ship.x + 3, ship.y, 5, 5));
+            lastPress = null;
         }
+        //shots movement & removal when hitting the top of the canvas
+        for(var i = 0, l = shots.length; i < l; i++) {
+            shots[i].y -= 10;
+            if(shots[i].y < 0) {
+                shots.splice(i--, 1);
+                l--;
+            }
+        }
+        //enemies movement
+        for(var i = 0; i < enemies.length; i++) {
+            enemies[i].y += 3;
+            if(enemies[i].y > canvas.height) {
+                enemies[i].x = random(canvas.width / 10) * 10;
+                enemies[i].y = 0;
+            }
+            //player-enemy intersection
+            if(ship.intersects(enemies[i])) {
+                pause = true;
+            }
+        }
+    
     }
 }
 
@@ -123,15 +179,23 @@ game.paint = function(context) {
 
     context.drawImage(shipImage, ship.x, ship.y);
 
+    context.fillStyle='#000';
+    for(var i=0,l=enemies.length;i<l;i++)
+    enemies[i].fill(context);
+
     for(var i = 0; i < shots.length; i++) {
         context.drawImage(shotImage, shots[i].x, shots[i].y);
+    }
+
+    if(pause) {
+        context.fillStyle = '#ff0080a1';
+        context.fillText('Pause', 150, 20);
     }
 }
 
 window.onload = function() {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
-    ship = new Rectangle(145, 130, 16, 15);
     shipImage.src = 'assets/ship.png';
     shotImage.src = 'assets/shot.png';
     run();
